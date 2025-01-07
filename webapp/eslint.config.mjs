50ffb4a23dae70d2a3fdfcefb5d106ddaa17a95a
Keita Ito <eguchi@eguchi.cc>
2025-01-07 16:01:02 +0900

    chore: eslintの構成

--- webapp/eslint.config.mjs
+++ webapp/eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,8 +12,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = tseslint.config(
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic
+);
 
 export default eslintConfig;
