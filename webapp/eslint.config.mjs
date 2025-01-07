dcf3be142744496a58998bc3d5dc359f3162195c
Keita Ito <eguchi@eguchi.cc>
2025-01-07 16:02:59 +0900

    chore: prettierのインストール

--- webapp/eslint.config.mjs
+++ webapp/eslint.config.mjs
@@ -1,9 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
 import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { dirname } from "path";
 import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,7 +16,8 @@ const eslintConfig = tseslint.config(
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslint.configs.recommended,
   tseslint.configs.strict,
-  tseslint.configs.stylistic
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
 );
 
 export default eslintConfig;
