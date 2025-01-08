881c770efa40026f6dee8432dd9de5e5d8c9f0d5
kanaru0928 <kanaru0928@gmail.com>
2025-01-08 16:52:59 +0900

    chore: appbarのリンクを追加

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,4 +1,12 @@
-import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import NextLink from "next/link";
 import { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
@@ -7,7 +15,14 @@ export default function Layout({ children }: PropsWithChildren) {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="h1">
-            Portal411
+            <Link
+              href="/"
+              color="inherit"
+              underline="none"
+              component={NextLink}
+            >
+              Portal411
+            </Link>
           </Typography>
         </Toolbar>
       </AppBar>
