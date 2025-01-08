acdfda3ddcdea5f3b0aa120759e925fae67cc103
kanaru0928 <kanaru0928@gmail.com>
2025-01-08 21:54:46 +0900

    chore: 余白を調整

--- src/app/(index)/members/page.tsx
+++ src/app/(index)/members/page.tsx
@@ -8,17 +8,11 @@ export default async function MembersPage() {
   const members = await prisma.member.findMany({ orderBy: { id: "asc" } });
 
   return (
-    <Box
-      sx={[
-        {
-          "&>*:not(:first-child)": {
-            mt: 2,
-          },
-        },
-      ]}
-    >
+    <Box>
       <Typography variant="h6">部員名簿</Typography>
-      <MembersTable members={members} />
+      <Box sx={{ mt: 2 }}>
+        <MembersTable members={members} />
+      </Box>
     </Box>
   );
 }
