6349f25bd4fefe42c34007cf3f98247cf4cee949
kanaru0928 <kanaru0928@gmail.com>
2025-01-10 12:58:19 +0900

    chore: 性癖

--- src/app/(index)/members/page.tsx
+++ src/app/(index)/members/page.tsx
@@ -6,11 +6,11 @@ export default async function MembersPage() {
   const members = await prisma.member.findMany({ orderBy: { id: "asc" } });
 
   return (
-    <Box>
+    <>
       <Typography variant="h6">部員名簿</Typography>
       <Box sx={{ mt: 2 }}>
         <MembersTable members={members} />
       </Box>
-    </Box>
+    </>
   );
 }
